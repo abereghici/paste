f00d29ceb2edadb3c6b7ba31aecc9370f4926c91
Glorili Alejandro <galejandro@twilio.com>
2021-08-24 13:31:16 -0700

    chore(website): add text content to status pattern

---
+++ packages/paste-website/src/component-examples/StatusPatternExamples.ts
@@ -0,0 +1,7 @@
+// import {Box} from '@twilio-paste/box';
+// import {Text} from '@twilio-paste/text';
+// import {ProcessDisabledIcon} from '@twilio-paste/icons/esm/ProcessDisabledIcon';
+
+export const processStatusExamples = () => {
+  return true;
+};
